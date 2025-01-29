ce53f77de252da9de7365c878753a0d346f49648
Alicia074 <aliciaesova@gmail.com>
2025-01-28 19:27:30 -0500

    Updated coral delivery motor names

--- src/main/java/frc/robot/subsystems/CoralDelivery/CoralDeliverySubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralDelivery/CoralDeliverySubsystem.java
@@ -13,56 +13,57 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralDeliverySubsystem extends SubsystemBase {
   /** Creates a new CoralDSubsystem. */
-  private final SparkMax coralDOne;
-  private final SparkMax coralDTwo;
-  private final SparkMax coralDThree;
+  private final SparkMax elevator;
+  private final SparkMax pivot;
+  private final SparkMax delivery;
 
-  private final RelativeEncoder coralDOneEncoder;
-  private final RelativeEncoder coralDTwoEncoder;
-  private final RelativeEncoder coralDThreeEncoder;
+  private final RelativeEncoder elevatorEncoder;
+  private final RelativeEncoder pivotEncoder;
+  private final RelativeEncoder deliveryEncoder;
 
   public CoralDeliverySubsystem() {
-    coralDOne = CoralDeliveryCfg.CORALD_ONE_MOTOR;
-    coralDTwo = CoralDeliveryCfg.CORALD_TWO_MOTOR;
-    coralDThree = CoralDeliveryCfg.CORALD_THREE_MOTOR;
+    elevator = CoralDeliveryCfg.CORALD_ONE_MOTOR;
+    pivot = CoralDeliveryCfg.CORALD_TWO_MOTOR;
+    delivery = CoralDeliveryCfg.CORALD_THREE_MOTOR;
 
-    coralDOneEncoder = coralDOne.getEncoder();
-    EncoderConfig coralDOneEncoderConfig = new EncoderConfig();
-    coralDOneEncoderConfig.positionConversionFactor(CoralDeliveryCfg.CORALD_ONE_GEAR_RATIO);
-    coralDOneEncoderConfig.velocityConversionFactor(CoralDeliveryCfg.CORALD_ONE_GEAR_RATIO);
+    elevatorEncoder = elevator.getEncoder();
+    EncoderConfig elevatorEncoderConfig = new EncoderConfig();
+    elevatorEncoderConfig.positionConversionFactor(CoralDeliveryCfg.CORALD_ONE_GEAR_RATIO);
+    elevatorEncoderConfig.velocityConversionFactor(CoralDeliveryCfg.CORALD_ONE_GEAR_RATIO);
 
-    coralDTwoEncoder = coralDTwo.getEncoder();
-    EncoderConfig coralDTwoEncoderConfig = new EncoderConfig();
-    coralDTwoEncoderConfig.positionConversionFactor(CoralDeliveryCfg.CORALD_TWO_GEAR_RATIO);
-    coralDTwoEncoderConfig.velocityConversionFactor(CoralDeliveryCfg.CORALD_TWO_GEAR_RATIO);
+    pivotEncoder = pivot.getEncoder();
+    EncoderConfig pivotEncoderConfig = new EncoderConfig();
+    pivotEncoderConfig.positionConversionFactor(CoralDeliveryCfg.CORALD_TWO_GEAR_RATIO);
+    pivotEncoderConfig.velocityConversionFactor(CoralDeliveryCfg.CORALD_TWO_GEAR_RATIO);
 
-    coralDThreeEncoder = coralDThree.getEncoder();
-    EncoderConfig coralDThreeEncoderConfig = new EncoderConfig();
-    coralDThreeEncoderConfig.positionConversionFactor(CoralDeliveryCfg.CORALD_THREE_GEAR_RATIO);
-    coralDThreeEncoderConfig.velocityConversionFactor(CoralDeliveryCfg.CORALD_THREE_GEAR_RATIO);
+    deliveryEncoder = delivery.getEncoder();
+    EncoderConfig deliveryEncoderConfig = new EncoderConfig();
+    deliveryEncoderConfig.positionConversionFactor(CoralDeliveryCfg.CORALD_THREE_GEAR_RATIO);
+    deliveryEncoderConfig.velocityConversionFactor(CoralDeliveryCfg.CORALD_THREE_GEAR_RATIO);
 
 
-    SparkMaxConfig CoralDOneConfig = new SparkMaxConfig();
-    CoralDOneConfig.idleMode(CoralDeliveryCfg.CORALD_ONE_IDLE_MODE);
-    CoralDOneConfig.inverted(CoralDeliveryCfg.CORALD_ONE_MOTOR_REVERSED);
-    CoralDOneConfig.smartCurrentLimit(CoralDeliveryCfg.CORALD_ONE_CURRENT_LIMIT);
+    SparkMaxConfig elevatorConfig = new SparkMaxConfig();
+    elevatorConfig.idleMode(CoralDeliveryCfg.CORALD_ONE_IDLE_MODE);
+    elevatorConfig.inverted(CoralDeliveryCfg.CORALD_ONE_MOTOR_REVERSED);
+    elevatorConfig.smartCurrentLimit(CoralDeliveryCfg.CORALD_ONE_CURRENT_LIMIT);
 
-    CoralDOneConfig.apply(coralDOneEncoderConfig);
+    elevatorConfig.apply(elevatorEncoderConfig);
 
-    SparkMaxConfig CoralDTwoConfig = new SparkMaxConfig();
-    CoralDTwoConfig.idleMode(CoralDeliveryCfg.CORALD_TWO_IDLE_MODE);
-    CoralDTwoConfig.inverted(CoralDeliveryCfg.CORALD_TWO_MOTOR_REVERSED);
-    CoralDTwoConfig.smartCurrentLimit(CoralDeliveryCfg.CORALD_TWO_CURRENT_LIMIT);
+    SparkMaxConfig pivotConfig = new SparkMaxConfig();
+    pivotConfig.idleMode(CoralDeliveryCfg.CORALD_TWO_IDLE_MODE);
+    pivotConfig.inverted(CoralDeliveryCfg.CORALD_TWO_MOTOR_REVERSED);
+    pivotConfig.smartCurrentLimit(CoralDeliveryCfg.CORALD_TWO_CURRENT_LIMIT);
 
-    CoralDTwoConfig.apply(coralDTwoEncoderConfig);
+    pivotConfig.apply(pivotEncoderConfig);
 
-    SparkMaxConfig CoralDThreeConfig = new SparkMaxConfig();
-    CoralDThreeConfig.idleMode(CoralDeliveryCfg.CORALD_THREE_IDLE_MODE);
-    CoralDThreeConfig.inverted(CoralDeliveryCfg.CORALD_THREE_MOTOR_REVERSED);
-    CoralDThreeConfig.smartCurrentLimit(CoralDeliveryCfg.CORALD_THREE_CURRENT_LIMIT);
+    SparkMaxConfig deliveryConfig = new SparkMaxConfig();
+    deliveryConfig.idleMode(CoralDeliveryCfg.CORALD_THREE_IDLE_MODE);
+    deliveryConfig.inverted(CoralDeliveryCfg.CORALD_THREE_MOTOR_REVERSED);
+    deliveryConfig.smartCurrentLimit(CoralDeliveryCfg.CORALD_THREE_CURRENT_LIMIT);
 
-    CoralDThreeConfig.apply(coralDThreeEncoderConfig);
+    deliveryConfig.apply(deliveryEncoderConfig);
     
+
   }
 
   @Override
@@ -70,28 +71,28 @@ public class CoralDeliverySubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public void setCoralDOnePower(double power){
-    coralDOne.set(power);
+  public void setElevatorPower(double power){
+    elevator.set(power);
   }
 
-  public void setCoralDTwoPower(double power){
-    coralDTwo.set(power);
+  public void setPivotPower(double power){
+    pivot.set(power);
   }
 
-  public void setCoralDThreePower(double power){
-    coralDThree.set(power);
+  public void setDeliveryPower(double power){
+    delivery.set(power);
   }
 
  
-  public double getCoralDOnePosition(){
-    return coralDOneEncoder.getPosition();
+  public double getElevatorPosition(){
+    return elevatorEncoder.getPosition();
   }
 
-  public double getCoralDTwoPosition(){
-    return coralDTwoEncoder.getPosition();
+  public double getPivotPosition(){
+    return pivotEncoder.getPosition();
   }
 
-  public double getCoralDThreePosition(){
-    return coralDThreeEncoder.getPosition();
+  public double getDeliveryPosition(){
+    return deliveryEncoder.getPosition();
   }
 }
