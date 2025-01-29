feefd4d27a34009a690fcd1a21a70d3aa9225412
KiwiTheGremlin <rachelpawlowski91@gmail.com>
2025-01-28 20:22:38 -0500

    Updated Intake Indexer Names

---
+++ src/main/java/frc/robot/subsystems/IntakeIndexer/IntakeIndexerCfg.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems.IntakeIndexer;
+
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
+public class IntakeIndexerCfg {
+     public static final int LEFTPIVOT_MOTOR_ID = 3;
+     public static final int LEFTINTAKE_MOTOR_ID = 3;
+     public static final int RIGHTPIVOT_MOTOR_ID = 3;
+     public static final int RIGHTINTAKE_MOTOR_ID = 3;
+     public static final int INDEXER_MOTOR_ID = 3;
+
+    public static final SparkMax LEFTPIVOT_MOTOR = new SparkMax(LEFTPIVOT_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+    public static final SparkMax LEFTINTAKE_MOTOR = new SparkMax(LEFTINTAKE_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+    public static final SparkMax RIGHTPIVOT_MOTOR = new SparkMax(RIGHTPIVOT_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+    public static final SparkMax RIGHTINTAKE_MOTOR = new SparkMax(RIGHTINTAKE_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+    public static final SparkMax INDEXER_MOTOR = new SparkMax(INDEXER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+
+    public static final double LEFTPIVOT_GEAR_RATIO = 1/25.0;//TBD with design
+    public static final double LEFTINTAKE_GEAR_RATIO = 1/25.0;//TBD with design
+    public static final double RIGHTPIVOT_GEAR_RATIO = 1/25.0;//TBD with design
+    public static final double RIGHTINTAKE_GEAR_RATIO = 1/25.0;//TBD with design
+    public static final double INDEXER_GEAR_RATIO = 1/9.0;//TBD with design
+
+    public static final SparkBaseConfig.IdleMode LEFTPIVOT_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
+    public static final SparkBaseConfig.IdleMode LEFTINTAKE_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
+    public static final SparkBaseConfig.IdleMode RIGHTPIVOT_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
+    public static final SparkBaseConfig.IdleMode RIGHTINTAKE_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
+    public static final SparkBaseConfig.IdleMode INDEXER_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
+    public static final boolean LEFTPIVOT_MOTOR_REVERSED = false;
+    public static final boolean LEFTINTAKE_MOTOR_REVERSED = false;
+    public static final boolean RIGHTPIVOT_MOTOR_REVERSED = false;
+    public static final boolean RIGHTINTAKE_MOTOR_REVERSED = false;
+    public static final boolean INDEXER_MOTOR_REVERSED = false;
+    public static final int LEFTPIVOT_CURRENT_LIMIT = 50;
+    public static final int LEFTINTAKE_CURRENT_LIMIT = 50;
+    public static final int RIGHTPIVOT_CURRENT_LIMIT = 50;
+    public static final int RIGHTINTAKE_CURRENT_LIMIT = 50;
+    public static final int INDEXER_CURRENT_LIMIT = 20;
+}
---
+++ src/main/java/frc/robot/subsystems/IntakeIndexer/IntakeIndexerSubsystem.java
@@ -0,0 +1,151 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+package frc.robot.subsystems.IntakeIndexer;
+
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.EncoderConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class IntakeIndexerSubsystem extends SubsystemBase {
+  /** Creates a new CoralDSubsystem. */
+  private final SparkMax leftPivot;
+  private final SparkMax leftIntake;
+  private final SparkMax rightPivot;
+  private final SparkMax rightIntake;
+  private final SparkMax indexer;
+
+
+  private final RelativeEncoder leftPivotEncoder;
+  private final RelativeEncoder leftIntakeEncoder;
+  private final RelativeEncoder rightPivotEncoder;
+  private final RelativeEncoder rightIntakeEncoder;
+  private final RelativeEncoder indexerEncoder;
+
+
+  public IntakeIndexerSubsystem() {
+    leftPivot = IntakeIndexerCfg.LEFTPIVOT_MOTOR;
+    leftIntake = IntakeIndexerCfg.LEFTINTAKE_MOTOR;
+    rightPivot = IntakeIndexerCfg.RIGHTPIVOT_MOTOR;
+    rightIntake = IntakeIndexerCfg.RIGHTINTAKE_MOTOR;
+    indexer = IntakeIndexerCfg.INDEXER_MOTOR;
+
+
+    leftPivotEncoder = leftPivot.getEncoder();
+    EncoderConfig leftPivotEncoderConfig = new EncoderConfig();
+    leftPivotEncoderConfig.positionConversionFactor(IntakeIndexerCfg.LEFTPIVOT_GEAR_RATIO);
+    leftPivotEncoderConfig.velocityConversionFactor(IntakeIndexerCfg.LEFTPIVOT_GEAR_RATIO);
+
+
+    leftIntakeEncoder = leftIntake.getEncoder();
+    EncoderConfig leftIntakeEncoderConfig = new EncoderConfig();
+    leftIntakeEncoderConfig.positionConversionFactor(IntakeIndexerCfg.LEFTINTAKE_GEAR_RATIO);
+    leftIntakeEncoderConfig.velocityConversionFactor(IntakeIndexerCfg.LEFTINTAKE_GEAR_RATIO);
+
+
+    rightPivotEncoder = rightPivot.getEncoder();
+    EncoderConfig rightPivotEncoderConfig = new EncoderConfig();
+    rightPivotEncoderConfig.positionConversionFactor(IntakeIndexerCfg.RIGHTPIVOT_GEAR_RATIO);
+    rightPivotEncoderConfig.velocityConversionFactor(IntakeIndexerCfg.RIGHTPIVOT_GEAR_RATIO);
+
+
+    rightIntakeEncoder = rightIntake.getEncoder();
+    EncoderConfig rightIntakeEncoderConfig = new EncoderConfig();
+    rightIntakeEncoderConfig.positionConversionFactor(IntakeIndexerCfg.RIGHTINTAKE_GEAR_RATIO);
+    rightIntakeEncoderConfig.velocityConversionFactor(IntakeIndexerCfg.RIGHTINTAKE_GEAR_RATIO);
+
+
+    indexerEncoder = indexer.getEncoder();
+    EncoderConfig indexerEncoderConfig = new EncoderConfig();
+    indexerEncoderConfig.positionConversionFactor(IntakeIndexerCfg.INDEXER_GEAR_RATIO);
+    indexerEncoderConfig.velocityConversionFactor(IntakeIndexerCfg.INDEXER_GEAR_RATIO);
+
+
+    SparkMaxConfig leftPivotConfig = new SparkMaxConfig();
+    leftPivotConfig.idleMode(IntakeIndexerCfg.LEFTPIVOT_IDLE_MODE);
+    leftPivotConfig.inverted(IntakeIndexerCfg.LEFTPIVOT_MOTOR_REVERSED);
+    leftPivotConfig.smartCurrentLimit(IntakeIndexerCfg.LEFTPIVOT_CURRENT_LIMIT);
+
+
+    SparkMaxConfig leftIntakeConfig = new SparkMaxConfig();
+    leftIntakeConfig.idleMode(IntakeIndexerCfg.LEFTINTAKE_IDLE_MODE);
+    leftIntakeConfig.inverted(IntakeIndexerCfg.LEFTINTAKE_MOTOR_REVERSED);
+    leftIntakeConfig.smartCurrentLimit(IntakeIndexerCfg.LEFTINTAKE_CURRENT_LIMIT);
+
+
+    SparkMaxConfig rightPivotConfig = new SparkMaxConfig();
+    rightPivotConfig.idleMode(IntakeIndexerCfg.RIGHTPIVOT_IDLE_MODE);
+    rightPivotConfig.inverted(IntakeIndexerCfg.RIGHTPIVOT_MOTOR_REVERSED);
+    rightPivotConfig.smartCurrentLimit(IntakeIndexerCfg.RIGHTPIVOT_CURRENT_LIMIT);
+
+
+    SparkMaxConfig rightIntakeConfig = new SparkMaxConfig();
+    rightIntakeConfig.idleMode(IntakeIndexerCfg.RIGHTINTAKE_IDLE_MODE);
+    rightIntakeConfig.inverted(IntakeIndexerCfg.RIGHTINTAKE_MOTOR_REVERSED);
+    rightIntakeConfig.smartCurrentLimit(IntakeIndexerCfg.RIGHTINTAKE_CURRENT_LIMIT);
+
+
+    SparkMaxConfig indexerConfig = new SparkMaxConfig();
+    indexerConfig.idleMode(IntakeIndexerCfg.INDEXER_IDLE_MODE);
+    indexerConfig.inverted(IntakeIndexerCfg.INDEXER_MOTOR_REVERSED);
+    indexerConfig.smartCurrentLimit(IntakeIndexerCfg.INDEXER_CURRENT_LIMIT);
+
+    leftPivotConfig.apply(leftPivotConfig);
+    leftIntakeConfig.apply(leftIntakeConfig);
+    rightPivotConfig.apply(rightPivotConfig);  
+    rightIntakeConfig.apply(rightIntakeConfig);
+    indexerConfig.apply(indexerConfig);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+  public void setLeftPivotPower(double power){
+    leftPivot.set(power);
+  }
+  public void setLeftIntakePower(double power){
+    leftIntake.set(power);
+  }
+  public void setRightPivotPower(double power){
+    rightPivot.set(power);
+  }
+  public void setRightIntakePower(double power){
+    rightIntake.set(power);
+  }
+  public void setIndexerPower(double power){
+    indexer.set(power);
+  }
+
+
+  public double getLeftPivotPosition(){
+    return leftPivotEncoder.getPosition();
+  }
+  public double getLeftIntakePosition(){
+    return leftIntakeEncoder.getPosition();
+  }
+  public double getRightPivotPosition(){
+    return rightPivotEncoder.getPosition();
+  }
+  public double getRightIntakePosition(){
+    return rightIntakeEncoder.getPosition();
+  }
+  public double getIndexerPosition(){
+    return indexerEncoder.getPosition();
+  }
+
+
+}
+
+
