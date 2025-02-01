510a95c7a6634b087c6497b1abbbc6e308066f4b
lesserc <cdlesser@provide.net>
2025-02-01 01:00:11 -0500

    Missed change for ReefscapeVision class

--- src/main/java/frc/robot/subsystems/Vision/ReefscapeVision.java
+++ src/main/java/frc/robot/subsystems/Vision/ReefscapeVision.java
@@ -1,9 +1,21 @@
 package frc.robot.subsystems.Vision;
 
+import frc.robot.subsystems.Vision.LimelightHelpers.LimelightResults;
+import frc.robot.subsystems.Vision.LimelightHelpers.LimelightTarget_Fiducial;
+
 public class ReefscapeVision {
 
+    private static LimelightResults jsonResults;
+
     public static int getReefTagId(){
+        //Default Tag ID to -1
         int tagId = -1;
+
+        if(LimelightHelpers.getTV("")){
+            jsonResults = LimelightHelpers.getLatestResults("");
+            LimelightTarget_Fiducial[] tags = jsonResults.targets_Fiducials;
+            tagId = (int)tags[0].fiducialID;
+        }
         return tagId;
     }
 
