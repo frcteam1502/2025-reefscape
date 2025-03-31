0a5ea25e45e52c6c08a41df8d9352a6aa5ebcdaf
Ankylogamer <ankylogamer1@gmail.com>
2025-03-31 19:02:39 -0400

    Elevator Set Pos Changes

--- src/main/java/frc/robot/subsystems/CoralDelivery/CoralDeliveryCfg.java
+++ src/main/java/frc/robot/subsystems/CoralDelivery/CoralDeliveryCfg.java
@@ -52,7 +52,7 @@ public class CoralDeliveryCfg {
     public static final boolean ELEVATOR_FOLLOWER_MOTOR_REVERSED = true;
     public static final boolean PIVOT_MOTOR_REVERSED = false;
     public static final boolean DELIVERY_MOTOR_REVERSED = false;
-    public static final boolean INDEXER_MOTOR_REVERSED = false;
+    public static final boolean INDEXER_MOTOR_REVERSED = true;
     public static final int ELEVATOR_CURRENT_LIMIT = 50;
     public static final int PIVOT_CURRENT_LIMIT = 40;
     public static final int DELIVERY_CURRENT_LIMIT = 30;
@@ -89,7 +89,7 @@ public class CoralDeliveryCfg {
 
     public static final int CORAL_PRESENT_THRESH_MM = 60;
 
-    public static final double ELEVATOR_LOAD_POSITION = 0.5;
+    public static final double ELEVATOR_LOAD_POSITION = 5;
     public static final double ELEVATOR_LONE_POSITION = 20;
     public static final double ELEVATOR_LTWO_POSITION = 47.5;
     public static final double ELEVATOR_LTHREE_POSITION = 88;
